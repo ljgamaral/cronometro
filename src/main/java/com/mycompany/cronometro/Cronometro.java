30a52184759fc81c36ea67377c7013676cd992e6
Leandro Junio <86161500+ljgamaral@users.noreply.github.com>
2025-04-16 10:32:03 -0300

    Funções dos botões de start, parar e point

--- src/main/java/com/mycompany/cronometro/Cronometro.java
+++ src/main/java/com/mycompany/cronometro/Cronometro.java
@@ -13,9 +13,29 @@ public class Cronometro extends Thread {
     private int seg = 0;
     private int milisRestantes = 0;
     private String timerFormatado;
+    private boolean iniciado = false;
+
+    public synchronized void startar() 
+        {if (!iniciado) {
+            this.start();
+            iniciado = true;
+        }
+        rodando = true;
+        this.notify(); // Notifica a thread para sair do wait()
+    }
 
     public void run() {
-        while (rodando) {
+        while (true) {
+            synchronized (this) {
+                while (!rodando) {
+                    try {
+                        this.wait(); // Espera até que seja notificada
+                    } catch (InterruptedException e) {
+                        return; // Encerra se a thread for interrompida
+                    }
+                }
+            }
+         
             minutos = (miliseg / 60000) % 60; // Cálculo correto dos minutos
             seg = (miliseg / 1000) % 60;      // Cálculo correto dos segundos
             milisRestantes = miliseg % 1000; // Milissegundos restantes
@@ -34,7 +54,15 @@ public class Cronometro extends Thread {
     public void parar() {
         rodando = false;
     }
-
+    
+    public void zerar() {
+        miliseg = 0;
+        minutos = 0;
+        seg = 0;
+        milisRestantes = 0;
+        timerFormatado = "00:00:000";
+    }
+    
     public String getTimer() {
         return timerFormatado;
     }
--- src/main/java/com/mycompany/cronometro/tela.java
+++ src/main/java/com/mycompany/cronometro/tela.java
@@ -6,16 +6,19 @@ import javax.swing.Timer;
 import java.util.Vector;
 
 public class tela extends WindowAdapter implements ActionListener{
-    private Vector vVoltas;
     private Frame janela;
     private Panel painelTimer,painelBotoes;
-    private Label lcronometro, lvoltas, ltotal;
-    private Button bstart, bpoint, bfim, brelatorio;
-    
+    private Label lcronometro, ltotal;
+    private Button bstart, bparar, bpoint, bfim, brelatorio;
+    private TextArea tvoltas;
+    private Vector vVoltas;
+        
     private Cronometro c;
+    private Timer timerSwing;
     
     public tela() {
-        vVoltas =new Vector();
+        c = new Cronometro();
+        vVoltas = new Vector();
         janela = new Frame();
         janela.setTitle("Cronometro");
         janela.setSize(370,414);
@@ -37,21 +40,17 @@ public class tela extends WindowAdapter implements ActionListener{
         
         lcronometro = new Label("00:00:000");
         lcronometro.setFont(new Font("Arial", Font.BOLD, 24));
-        lcronometro.setBounds(100, 80, 200, 40);
+        lcronometro.setBounds(100, 10, 200, 40);
+        tvoltas = new TextArea("", 30, 30, TextArea.SCROLLBARS_NONE);
+        tvoltas.setBounds(50, 50, 200, 150);
+        tvoltas.setEditable(false);
         painelTimer.add(lcronometro);
-        
-        c = new Cronometro();
-        c.start();       
-        
-        Timer timerSwing = new Timer(100, new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                lcronometro.setText(c.getTimer());
-            }
-        });
-        timerSwing.start();
-        
+        painelTimer.add(tvoltas);
+                        
         bstart = new Button("START");
         bstart.addActionListener(this);
+        bparar = new Button("PARAR");
+        bparar.addActionListener(this);
         bpoint = new Button("POINT");
         bpoint.addActionListener(this);
         bfim = new Button("FIM");
@@ -60,6 +59,7 @@ public class tela extends WindowAdapter implements ActionListener{
         brelatorio.addActionListener(this);
         
         painelBotoes.add(bstart);
+        painelBotoes.add(bparar);
         painelBotoes.add(bpoint);
         painelBotoes.add(bfim);
         painelBotoes.add(brelatorio);
@@ -72,9 +72,56 @@ public class tela extends WindowAdapter implements ActionListener{
     }
     
     public void actionPerformed(ActionEvent e) {
-        
+        Button b=(Button)e.getSource();
+        if (b==bstart) {
+            this.botaoStart();
+        } else if (b==bparar) {
+            this.botaoParar();
+        } else if (b==bpoint) {
+            this.botaoPoint();
+        } else if (b==bfim) {
+            this.botaoFim();
+        }
     }
     
+    void voltasFormatadas(Vector vVoltas) {
+        String formatoAntes = "";
+        for (int i = 0; i < vVoltas.size(); i++) {
+            formatoAntes += (i+1) + " - " + vVoltas.get(i) + "\n";
+        }
+        tvoltas.setText(formatoAntes);
+    }
+    
+    void botaoStart () {
+        c.startar();
+        timerSwing = new Timer(1, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                lcronometro.setText(c.getTimer());
+            }
+        });
+        timerSwing.start();
+    }
+    
+    void botaoParar() {
+        c.parar();
+        timerSwing.stop();
+    }
+    
+    void botaoPoint() {
+        vVoltas.addElement(c.getTimer());
+        voltasFormatadas(vVoltas);
+    }
+    
+    void botaoFim() {
+        c.parar();
+        c.zerar();
+        lcronometro.setText(c.getTimer());
+        timerSwing.stop();
+    }
+    
+    public void windowClosing(WindowEvent e) {
+        System.exit(0);
+    }
     
     public static void main(String[] args) {
         tela t = new tela();
